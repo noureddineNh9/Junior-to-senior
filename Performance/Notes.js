c3a7766e4e3541b98bae2fa1789752674428655c
noureddine haddad <noureddinehaddad17@gmail.com>
2021-11-19 00:42:24 +0100

    performance 1.0

---
+++ Performance/Notes.js
@@ -0,0 +1,8 @@
+/**
+ * ! 3 keys to Performance
+ *
+ * > improve what happens on the client side (frontend).
+ * > improve the transfer of our files over the wire network latency.
+ * > improve the processing done in the backend.
+ *
+ */
--- ssh.js
+++ ssh.js
@@ -1,8 +1,15 @@
 /**
  * - SSH
- * 
+ *
  * to connect to a computer 'ssh user@ipadresse/domaine_name'
- * 
- * 
- * 
+ *
+ * * Symmetrical Encryption
+ *    using key to encrypte and decrypte message
+ *
+ * * Asymmetrical Encryption
+ *
+ *  every computer have public key and private key ,
+ *  to send data from computer A to computer B you should have the public key for computer B
+ *  and computer B can read this data by his private key.
+ *
  */
